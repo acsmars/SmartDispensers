c2fd89259f0ee130eeaa2f0d4cdcc47594aa6eb0
Austin Schafer <acsmars@gmail.com>
2023-06-29 17:54:04 -0700

    Break snow with silk touch when applicable

--- src/main/java/com/acsmars/smartdispensers/interactions/BreakWithTool.java
+++ src/main/java/com/acsmars/smartdispensers/interactions/BreakWithTool.java
@@ -5,6 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.type.Snow;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
@@ -21,12 +22,19 @@ public class BreakWithTool implements Interaction {
     @Override
     public boolean performInteraction(SmartDispensers plugin, BlockDispenseEvent event, ItemStack sourceItem, Block targetBlock) {
         if (targetBlock.getType().equals(Material.SNOW)) {
-            // There was no chest with space for the bucket.
+
             try {
                 int layerCount = ((Snow) targetBlock.getBlockData()).getLayers();
 
                 Location dropLocation = targetBlock.getLocation();
-                dropLocation.getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.SNOWBALL, layerCount));
+                if (sourceItem.getItemMeta().hasEnchant(Enchantment.SILK_TOUCH)) {
+                    dropLocation.getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.SNOW, layerCount));
+                } else {
+                    dropLocation.getWorld().dropItemNaturally(dropLocation, new ItemStack(Material.SNOWBALL, layerCount));
+                }
+                targetBlock.setType(Material.AIR);
+                event.setCancelled(true);
+                return true;
             } catch (NullPointerException ignored) {
             }
         }
